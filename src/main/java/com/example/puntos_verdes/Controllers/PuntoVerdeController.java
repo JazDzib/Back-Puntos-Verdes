a4a6cc08210d696cde43c95ddd3068a6e5e7fae1
JazDzib <a22216871@alumnos.uady.mx>
2025-06-12 18:31:58 -0600

    Update PuntoVerdeController

--- src/main/java/com/example/puntos_verdes/Controllers/PuntoVerdeController.java
+++ src/main/java/com/example/puntos_verdes/Controllers/PuntoVerdeController.java
@@ -4,6 +4,9 @@ import com.example.puntos_verdes.Entity.PuntoVerde;
 import com.example.puntos_verdes.Service.PuntoVerdeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/PuntoVerde")
 @Tag(name  =  "Administracion de puntos verdes", description =  "Controlador de actividades realcionadas a los puntos verdes")
+@RequiredArgsConstructor
 public class PuntoVerdeController {
 
     @Autowired
@@ -25,12 +29,16 @@ public class PuntoVerdeController {
     }
 
     @PostMapping("insertPV")
-     public void insertPuntoVerde(@RequestBody PuntoVerde puntoVerde){
+    @Transactional
+     public ResponseEntity<Void> insertPuntoVerde(@RequestBody  PuntoVerde puntoVerde){
         puntoVerdeService.createPuntoVerde(puntoVerde);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("{puntoId}")
-    public void deletePuntoVerde(@PathVariable Long puntoId){
+    @Transactional
+    public ResponseEntity<Void> deletePuntoVerde(@PathVariable Long puntoId){
         puntoVerdeService.deletePuntoVerde(puntoId);
+        return ResponseEntity.noContent().build();
     }
 }
